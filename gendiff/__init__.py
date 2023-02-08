5413f0992c19b83dfce407daf5d971acb5a37bc3
tyranich <63128958+tyranich@users.noreply.github.com>
2023-02-08 23:06:50 +0300

    lots a fixed

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -1,1 +1,2 @@
 from gendiff.tree import generate_diff  # noqa: F401
+from gendiff.cli import parse_args
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,9 +1,9 @@
-from gendiff.cli import parser_args
+from gendiff.cli import parse_args
 from gendiff.tree import generate_diff
 
 
 def main():
-    args = parser_args()
+    args = parse_args()
     formater = args.format
     generate_diff(args.first_file, args.second_file, formater=formater)
 
