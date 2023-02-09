4080439fcbffef52ed2b3c97d445fc3343a22d1c
tyranich <63128958+tyranich@users.noreply.github.com>
2023-02-09 21:02:46 +0300

    correction test hexlet chek

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -9,39 +9,39 @@ FILEMODE = 'tree'
 @pytest.mark.parametrize("file1, file2, result",
                          [
                             pytest.lazy_fixture(('file1_tree_json_path',
-                            'file2_tree_json_path', 'result_render')),
+                                'file2_tree_json_path', 'result_render')),
                             pytest.lazy_fixture(('file1_tree_yml_path',
-                            'file2_tree_yml_path', 'result_render'))
+                                'file2_tree_yml_path', 'result_render'))
                          ])
 def tests_plain(file1, file2, result):
     assert result == generate_diff(file1, file2, formater=FORMATTER)
 
 
 # test json
-FORMATTER='json'
+FORMATTER ='json'
 
 
 @pytest.mark.parametrize("file1, file2, result",
                          [
                             pytest.lazy_fixture(('file1_tree_json_path',
-                            'file2_tree_json_path', 'result_render')),
+                                'file2_tree_json_path', 'result_render')),
                             pytest.lazy_fixture(('file1_tree_yml_path',
-                            'file2_tree_yml_path', 'result_render'))
+                                'file2_tree_yml_path', 'result_render'))
                          ])
 def tests_json(file1, file2, result):
     assert result == generate_diff(file1, file2, formater=FORMATTER)
 
 
 # test stylish
-FORMATTER='stylish'
+FORMATTER ='stylish'
 
 
 @pytest.mark.parametrize("file1, file2, result",
                          [
                             pytest.lazy_fixture(('file1_tree_json_path',
-                            'file2_tree_json_path', 'result_render')),
+                                'file2_tree_json_path', 'result_render')),
                             pytest.lazy_fixture(('file1_tree_yml_path',
-                            'file2_tree_yml_path', 'result_render'))
+                                'file2_tree_yml_path', 'result_render'))
                          ])
 def tests_stylish(file1, file2, result):
     assert result == generate_diff(file1, file2, formater=FORMATTER)
