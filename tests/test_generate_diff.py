4277e38df06fe2b3d4c22f6322fffd084ba16ff1
tyranich <63128958+tyranich@users.noreply.github.com>
2023-02-09 20:57:50 +0300

    correction test hexlet chek

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -5,39 +5,43 @@ FORMATTER = 'plain'
 FILEMODE = 'tree'
 
 
-#test plain
+# test plain
 @pytest.mark.parametrize("file1, file2, result",
-                        [
+                         [
                             pytest.lazy_fixture(('file1_tree_json_path',
                             'file2_tree_json_path', 'result_render')),
                             pytest.lazy_fixture(('file1_tree_yml_path',
                             'file2_tree_yml_path', 'result_render'))
-                        ])
+                         ])
 def tests_plain(file1, file2, result):
     assert result == generate_diff(file1, file2, formater=FORMATTER)
 
 
-#test json
+# test json
 FORMATTER='json'
+
+
 @pytest.mark.parametrize("file1, file2, result",
-                        [
+                         [
                             pytest.lazy_fixture(('file1_tree_json_path',
                             'file2_tree_json_path', 'result_render')),
                             pytest.lazy_fixture(('file1_tree_yml_path',
                             'file2_tree_yml_path', 'result_render'))
-                        ])
+                         ])
 def tests_json(file1, file2, result):
     assert result == generate_diff(file1, file2, formater=FORMATTER)
 
 
-#test stylish
+# test stylish
 FORMATTER='stylish'
+
+
 @pytest.mark.parametrize("file1, file2, result",
-                        [
+                         [
                             pytest.lazy_fixture(('file1_tree_json_path',
                             'file2_tree_json_path', 'result_render')),
                             pytest.lazy_fixture(('file1_tree_yml_path',
                             'file2_tree_yml_path', 'result_render'))
-                        ])
+                         ])
 def tests_stylish(file1, file2, result):
     assert result == generate_diff(file1, file2, formater=FORMATTER)
